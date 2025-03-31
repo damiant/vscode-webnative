f4bdfa193f66224881471cafbeb6e1b224f2c591
damiantarnawsky <damiantarnawsky@gmail.com>
2025-03-31 16:58:02 -0700

    fix: refactor capacitor integrate web project detect of folder

--- src/rules-web-project.ts
+++ src/rules-web-project.ts
@@ -1,38 +1,23 @@
-import { exists } from './analyzer';
+import { exists, isGreaterOrEqual } from './analyzer';
 import { InternalCommand } from './command-name';
 import { MonoRepoType } from './monorepo';
 
 import { npmInstall, npx } from './node-commands';
 import { Project } from './project';
-import { Tip, TipType } from './tip';
-import { asAppId } from './utilities';
+import { QueueFunction, Tip, TipType } from './tip';
+import { asAppId, openUri } from './utilities';
 import { checkCapacitorPluginMigration } from './rules-capacitor-plugins';
 import { existsSync } from 'fs';
 import { join } from 'path';
+import { readAngularJson } from './rules-angular-json';
+import { runCommands } from './advanced-actions';
+import { window } from 'vscode';
 
 /**
  * Web projects are not using Capacitor or Cordova
  * @param  {Project} project
  */
 export function webProject(project: Project) {
-  let outFolder = 'www';
-
-  // If there is a build folder and not a www folder then...
-  if (!existsSync(join(project.projectFolder(), 'www'))) {
-    if (existsSync(join(project.projectFolder(), 'build')) || exists('react')) {
-      outFolder = 'build'; // use build folder (usually react)
-    } else if (existsSync(join(project.projectFolder(), 'dist')) || exists('vue')) {
-      outFolder = 'dist'; /// use dist folder (usually vue)
-    }
-    if (exists('@ionic/angular') || exists('ionicons')) {
-      // Likely www
-    } else {
-      outFolder = 'dist';
-    }
-  }
-
-  const pre = project.repoType != MonoRepoType.none ? InternalCommand.cwd : '';
-
   if (project.isCapacitorPlugin) {
     checkCapacitorPluginMigration(project);
   }
@@ -44,17 +29,77 @@ export function webProject(project: Project) {
         '',
         TipType.Capacitor2,
         'Integrate Capacitor with this project to make it native mobile?',
-        [
-          npmInstall(`@capacitor/core`),
-          npmInstall(`@capacitor/cli`),
-          npmInstall(`@capacitor/app @capacitor/haptics @capacitor/keyboard @capacitor/status-bar`),
-          `${pre}${npx(project)} capacitor init "${project.name}" "${asAppId(project.name)}" --web-dir ${outFolder}`,
-          InternalCommand.ionicInit,
-        ],
+        undefined,
         'Add Capacitor',
         'Capacitor added to this project',
-        'https://capacitorjs.com',
-      ).canIgnore(),
+      )
+        .canIgnore()
+        .showProgressDialog()
+        .setQueuedAction(async (queueFunction: QueueFunction) => {
+          queueFunction();
+          await integrateCapacitor(project);
+        }),
     );
+  }
+}
+
+async function integrateCapacitor(project: Project) {
+  const result = await window.showInformationMessage(
+    `Integrate Capacitor with this project to make it native mobile?`,
+    'Add',
+    'Info',
+    'Exit',
+  );
+  if (result === 'Info') {
+    openUri('https://capacitorjs.com');
+    return;
+  }
+  if (result !== 'Add') {
+    return;
+  }
+
+  let outFolder = 'www';
+  if (exists('@ionic/angular') || exists('ionicons')) {
+    // Likely www
+  } else {
+    outFolder = 'dist';
+  }
+  // If there is a build folder and not a www folder then...
+  if (!existsSync(join(project.projectFolder(), 'www'))) {
+    if (existsSync(join(project.projectFolder(), 'build')) || exists('react')) {
+      outFolder = 'build'; // use build folder (usually react)
+    } else if (exists('@angular/core') && isGreaterOrEqual('@angular/core', '18.0.0')) {
+      outFolder = guessOutputFolder(project);
+    } else if (existsSync(join(project.projectFolder(), 'dist')) || exists('vue')) {
+      outFolder = 'dist'; /// use dist folder (usually vue)
+    }
+  }
+
+  const pre = project.repoType != MonoRepoType.none ? InternalCommand.cwd : '';
+  await runCommands(
+    [
+      npmInstall(`@capacitor/core`),
+      npmInstall(`@capacitor/cli`),
+      npmInstall(`@capacitor/app @capacitor/haptics @capacitor/keyboard @capacitor/status-bar`),
+      `${pre}${npx(project)} capacitor init "${project.name}" "${asAppId(project.name)}" --web-dir ${outFolder}`,
+      InternalCommand.ionicInit,
+    ],
+    'Integrating Capacitor',
+    project,
+  );
+}
+
+// Read the output folder from angular.json
+function guessOutputFolder(project: Project): string {
+  try {
+    const angular = readAngularJson(project);
+    for (const projectName of Object.keys(angular.projects)) {
+      const outputPath = angular.projects[projectName].architect.build.options.outputPath;
+      if (outputPath) {
+        return outputPath;
+      }
+    }
+  } catch {
+    return 'dist';
   }
 }
