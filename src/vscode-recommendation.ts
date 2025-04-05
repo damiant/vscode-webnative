7fe224442a90176504c3c711f285a1ba7f90aa8d
damiantarnawsky <damiantarnawsky@gmail.com>
2025-04-05 10:49:12 -0700

    fix: cleanup recommendations

--- src/vscode-recommendation.ts
+++ src/vscode-recommendation.ts
@@ -1,7 +1,23 @@
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
+import { getSetting, setSetting, WorkspaceSetting } from './workspace-state';
 
-export function recommendWebNativeProject(folder: string): void {
+export function checkRecommendedExtensions(folder: string): void {
+  const recFile = join(folder, '.vscode', 'extensions.json');
+  if (getSetting(WorkspaceSetting.recCheck) == true) {
+    return;
+  }
+  if (existsSync(recFile)) {
+    const data = readFileSync(recFile, 'utf8');
+    const jsonData = JSON.parse(data);
+    jsonData.recommendations = jsonData.recommendations.filter((ext: string) => ext !== 'ionic.ionic');
+    jsonData.recommendations.push('Webnative.webnative');
+    writeFileSync(recFile, JSON.stringify(jsonData, null, 2), 'utf8');
+  }
+  setSetting(WorkspaceSetting.recCheck, true);
+}
+
+export function recommendWebNativeExtension(folder: string): void {
   const recFile = join(folder, '.vscode', 'extensions.json');
   try {
     if (!existsSync(recFile)) {
